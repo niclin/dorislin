fb44d7809a37a3f4d3fe3a70b98e215380518b83
Nic <bboyceo@hotmail.com>
2017-09-07 13:23:05 +0800

    Add SEO

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,10 +2,16 @@ class GalleriesController < ApplicationController
 
   def index
     @galleries = Gallery.where(is_hidden: false).rank(:row_order).all
+
+    set_page_title "Portfolio - 作品集"
   end
 
   def show
     @gallery = Gallery.find(params[:id])
+
+    set_page_title @gallery.title
+    set_page_description @gallery.description
+    set_page_image "https://dorislin.info#{@gallery.image.url}"
   end
 
   def about
