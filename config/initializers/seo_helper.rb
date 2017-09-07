b74666510b98778a6639ad248d518bf85807a36e
Nic <bboyceo@hotmail.com>
2017-09-07 13:33:09 +0800

    Minor adjustment

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -1,6 +1,6 @@
 SeoHelper.configure do |config|
   config.skip_blank               = false
-  config.site_name = "Doris Lin - 林詩晏作品集"
+  config.site_name = "Doris Lin - 林詩晏"
   config.default_page_description = "我是一名 UI 設計師，有一年以上的網站 UI 設計經驗，會手繪、電繪、平面設計，對於美感有自己的執著。"
   config.default_page_keywords    = "portfolio,ui,ux,design,3dmax,autocad,photoshop,illustrator"
   config.default_page_image = "https://dorislin.me/img/about.jpg"
